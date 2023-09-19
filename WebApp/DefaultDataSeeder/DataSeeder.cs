cfb9456319252f2817f48640fe512e6109878699
common khadka <commonkhadka@gmail.com>
2023-09-19 23:43:03 +0545

    Added Common Boiler Plate

---
+++ WebApp/DefaultDataSeeder/DataSeeder.cs
@@ -0,0 +1,48 @@
+﻿using DomainModule.Entity;
+using InfrastructureModule.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.DefaultDataSeeder
+{
+    public class DataSeeder
+    {
+  
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.EnsureCreated();
+                var RoleSuperAdmin = User.TypeSuperAdmin;
+                var RoleGeneral = User.TypeGeneral;
+                //Roles
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync(RoleSuperAdmin))
+                    await roleManager.CreateAsync(new IdentityRole(RoleSuperAdmin));
+                
+                if (!await roleManager.RoleExistsAsync(RoleGeneral))
+                    await roleManager.CreateAsync(new IdentityRole(RoleGeneral));
+               
+                //Users
+                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                string adminUserEmail = "admin@gmail.com";
+
+                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+                if (adminUser == null)
+                {
+                    var newAdminUser = new User("admin","admin", adminUserEmail, User.TypeSuperAdmin)
+                    {
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(newAdminUser, "admin");
+                    await userManager.AddToRoleAsync(newAdminUser, RoleSuperAdmin);
+                }
+
+
+
+            }
+        }
+    }
+}
